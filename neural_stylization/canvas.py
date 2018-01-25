515b4e6c08d65bef49f66415f95161d96e9def1f
Christian Kauten <Kautenja@users.noreply.github.com>
2018-01-24 21:31:57 -0600

    update images

--- neural_stylization/canvas.py
+++ neural_stylization/canvas.py
@@ -10,25 +10,35 @@ class Canvas(object):
     # the template for the repr method for this object
     REPR = "{}(content_path='{}', style_path='{}')"
 
-    def __init__(self, content_path: str, style_path: str) -> None:
+    def __init__(self,
+                content_path: str,
+                style_path: str,
+                dimensions: tuple=None) -> None:
         """
         A canvas determining where to get content & style and where to save.
 
         Args:
             content_path: the path to the image to use as content
             style_path: the path to the image to use for style
+            dimensions: a tuple of (width, height) determining optional dims
 
         Returns: None
         """
         self.content_path = content_path
         self.style_path = style_path
 
-        # load the content image
-        self.content_image = load_image(content_path)
-        self.content = normalize(image_to_matrix(self.content_image))
-
-        # store the height of the canvas based on the content size
-        self.height, self.width = self.content.shape[1], self.content.shape[2]
+        if dimensions is None:
+            # load the content image
+            self.content_image = load_image(content_path)
+            self.content = normalize(image_to_matrix(self.content_image))
+            # store the dimensions of the canvas based on the content size
+            self.height, self.width = self.content.shape[1], self.content.shape[2]
+        else:
+            # set the dimensions of the canvas based on the parameter
+            self.width, self.height = dimensions
+            # load the content image with the custom size
+            self.content_image = load_image(content_path, (self.width, self.height))
+            self.content = normalize(image_to_matrix(self.content_image))
 
         # load the style image (using the dimensions of the content)
         self.style_image = load_image(style_path, (self.width, self.height))
